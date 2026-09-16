ae131f5dc7e3bb84a4f2d93d2a211c62b87c90e9
cynicalmindset <yashraj.02594656@gmail.com>
2026-09-16 12:52:48 +0530

    msg

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./auth";
 import roomsRouter from './routes/rooms.ts';
+import messagesRouter from "./routes/message.ts";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 app.use("/api/rooms", roomsRouter);
+app.use("/api/rooms/:roomId/messages", messagesRouter);
 
 app.get("/health", (_req, res) => {
   res.json({
---
+++ src/routes/message.ts
@@ -0,0 +1,96 @@
+import { Router } from "express";
+import { auth } from "../auth";
+import { prisma } from "../lib/prisma";
+const router = Router({ mergeParams: true });
+
+//message get
+router.get("/", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+  if (!session?.user) {
+    return res.status(401).json({
+      error: "not logged in",
+    });
+  }
+
+  const { roomId } = req.params as { roomId: string };
+
+  const memebership = await prisma.roomMember.findUnique({
+    where: {
+      userId_roomId: {
+        userId: session.user.id,
+        roomId,
+      },
+    },
+  });
+
+  if (!memebership) {
+    return res.status(403).json({
+      error: "You are not a member of this room",
+    });
+  }
+
+  const message = await prisma.message.findMany({
+    where: {
+      roomId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  return res.json({
+    message,
+  });
+});
+
+//messsage post
+router.post("/", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+  if (!session?.user) {
+    return res.status(401).json({
+      error: "Not logged in",
+    });
+  }
+
+  const { roomId } = req.params as { roomId: string };
+  const { content } = req.body;
+
+  if (!content || typeof content !== "string") {
+    return res.status(400).json({
+      error: "message content is required",
+    });
+  }
+
+  const memebership = await prisma.roomMember.findUnique({
+    where: {
+      userId_roomId: {
+        userId: session.user.id,
+        roomId,
+      },
+    },
+  });
+
+  if (!memebership) {
+    return res.status(403).json({
+      error: "not memeber of this room",
+    });
+  }
+
+  const message = await prisma.message.create({
+    data: {
+      content: content.trim(),
+      userId: session.user.id,
+      roomId,
+    },
+  });
+
+  return res.json({
+    message,
+  });
+});
+
+export default router;
